8a5d3c4196291f245e170ebf9f3c06fc61ae635d
Anna Duma <aduma@uw.edu>
2017-07-16 11:18:53 -0700

    Changes API URL to proxy link

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-const apiURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather"
-const api = "93d3fc2c78eb66d0e26103026ba3c019"
+const apiURL = "https://uwpce-weather-proxy.herokuapp.com/data/2.5/weather"
+const apiId = "93d3fc2c78eb66d0e26103026ba3c019"
 
 const seattleLat = "47.6762"
 const seattleLon = "-122.3182"
@@ -8,11 +8,11 @@ const londonLat = "51.5074"
 const londonLon = "0.1278"
 
 function getSeattle () {
-  getWeather(apiURL + "?lat=" + seattleLat + "&lon=" + seattleLon + "&APPID=" + api)
+  getWeather(apiURL + "?lat=" + seattleLat + "&lon=" + seattleLon + "&APPID=" + apiId)
 }
 
 function getLondon () {
-  getWeather(apiURL + "?lat=" + londonLat + "&lon=" + londonLon + "&APPID=" + api)
+  getWeather(apiURL + "?lat=" + londonLat + "&lon=" + londonLon + "&APPID=" + apiId)
 }
 
 function getLocation() {
@@ -20,7 +20,7 @@ function getLocation() {
 }
 
 function myWeather (position) {
-  getWeather(apiURL + "?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=" + api)
+  getWeather(apiURL + "?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=" + apiId)
 }
 
 function getWeather(url) {
@@ -33,9 +33,13 @@ function getWeather(url) {
     let weatherDiv = document.getElementById("weather")
 
     let response = (JSON.parse(request.response))
-    console.log(response.wind.speed)
+    console.log(request.response)
 
-    weatherDiv.innerHTML = ("Temperature: " + (response.main.temp - 273.15).toFixed(1) + " degrees Celsius.<br>" +  "Today's forecast: " + response.weather[0].main + "<br>" + "Description: " + response.weather[0].description + "<br>" + "Humidity: " + response.main.humidity + "%<br>" + "Wind: " + response.wind.speed + "km/h.")
+    weatherDiv.innerHTML = ("Temperature: " + (response.body.main.temp - 273.15).toFixed(1) + " degrees Celsius.<br>" +
+                            "Today's forecast: " + response.body.weather[0].main + "<br>" +
+                            "Description: " + response.body.weather[0].description + "<br>" +
+                            "Humidity: " + response.body.main.humidity + "%<br>" +
+                            "Wind: " + response.body.wind.speed + "km/h.")
   }
 
   request.onerror = function (errorObj) {
